f877b2aca5184ecb38db304dd7a404e7f1b83fde
Anna Sova <sova.ania@gmail.com>
2023-02-06 12:30:29 +0100

    task 1, 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector(`body`);
+let intervalId;
+
+stopBtn.disabled = true;
+
+startBtn.addEventListener(`click`, startChangeColor);
+stopBtn.addEventListener(`click`, stopChangeColor);
+
+function changeColor() {
+    const randomHexColor = getRandomHexColor();
+    body.style.backgroundColor = randomHexColor;
+};
+
+function startChangeColor() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    intervalId = setInterval(changeColor, 1000);
+};
+
+function stopChangeColor() {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(intervalId);
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
