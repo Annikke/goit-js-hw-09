d4ad9b5144c25f51ae262ee79788ddc85f15a463
Annikke <111438425+Annikke@users.noreply.github.com>
2023-02-08 16:25:24 +0000

    Deploying to gh-pages from @ Annikke/goit-js-hw-09@e6bf32fcdf7f1fb276433057d17d744f6eed7581 🚀

---
+++ 01-color-switcher.17ba4b9e.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");let n;function o(){const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;d.style.backgroundColor=t}e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval(o,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.17ba4b9e.js.map
